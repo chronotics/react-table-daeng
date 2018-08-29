98e86ebdfdd264b2754346e1430647a2badbf5e4
daengdaenglee <gunho1020@gmail.com>
2018-08-30 08:48:36 +0900

    ENH: Add row height prop

--- src/components/2-molecules/table/index.js
+++ src/components/2-molecules/table/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 const Container = styled.div.attrs({
   style: props => ({ width: props.width, height: props.height }),
@@ -26,19 +26,16 @@ const TableBody = styled.div.attrs({
   flex-direction: column;
 `;
 
-const Row = styled.div`
+const Row = styled.div.attrs({
+  style: props => ({ height: props.height, minHeight: props.height }),
+})`
   display: flex;
-  min-height: 50px;
 `;
 
 const Cell = styled.div.attrs({
   style: props => ({ width: props.width, minWidth: props.width }),
 })`
-  ${props =>
-    props.backgroundColor &&
-    css`
-      background-color: ${props.backgroundColor};
-    `};
+  background-color: ${props => props.backgroundColor};
 `;
 
 class Table extends Component {
@@ -59,14 +56,15 @@ class Table extends Component {
       headHeight,
       onClickCell,
     } = this.props;
-    const defaultCellWidth = '100px';
+    const defaultWidth = '100px';
+    const defaultHeight = '50px';
     return (
       <Container width={width} height={height} innerRef={tableContainer}>
         <TableHead innerRef={tableHeader} height={headHeight}>
           {columns.map(col => (
             <Cell
               key={col.key}
-              width={col.width || defaultCellWidth}
+              width={col.width || defaultWidth}
               backgroundColor="white"
               onClick={event => onClickCell({ event, type: 'col', col })}
             >
@@ -76,11 +74,11 @@ class Table extends Component {
         </TableHead>
         <TableBody marginTop={headHeight}>
           {rows.map(row => (
-            <Row key={row.key}>
+            <Row key={row.key} height={row.height || defaultHeight}>
               {columns.map(col => (
                 <Cell
                   key={col.key}
-                  width={col.width || defaultCellWidth}
+                  width={col.width || defaultWidth}
                   onClick={event =>
                     onClickCell({ event, type: 'cell', row, col })
                   }
@@ -133,6 +131,7 @@ Table.propTypes = {
   rows: PropTypes.arrayOf(
     PropTypes.shape({
       key: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      height: PropTypes.string,
     }),
   ),
   width: PropTypes.string,
