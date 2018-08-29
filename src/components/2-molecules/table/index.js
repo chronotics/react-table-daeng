6dfa95834f4be4264f8b577c73faaa4596da7e2e
daengdaenglee <gunho1020@gmail.com>
2018-08-29 17:44:57 +0900

    ENH: Fixed table head for vertical scroll

--- src/components/2-molecules/table/index.js
+++ src/components/2-molecules/table/index.js
@@ -1,25 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
-const Table = ({ columns, rows, width, height }) => (
-  <table style={{ width, height }}>
-    <thead>
-      <tr>
-        {columns.map(col => (
-          <th key={col.key}>{col.title}</th>
-        ))}
-      </tr>
-    </thead>
-    <tbody>
-      {rows.map(row => (
-        <tr key={row.key}>
+const Container = styled.div.attrs({
+  style: props => ({ width: props.width, height: props.height }),
+})`
+  overflow: auto;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TableHeader = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  background-color: white;
+`;
+
+const TableBody = styled.div`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Row = styled.div`
+  display: flex;
+  min-height: 50px;
+`;
+
+const Cell = styled.div.attrs({
+  style: props => ({ width: props.width }),
+})``;
+
+class Table extends Component {
+  constructor(props) {
+    super(props);
+    this.tableContainer = React.createRef();
+    this.tableHeader = React.createRef();
+  }
+
+  render() {
+    const { tableContainer, tableHeader } = this;
+    const { columns, rows, width, height } = this.props;
+    return (
+      <Container width={width} height={height} innerRef={tableContainer}>
+        <TableHeader innerRef={tableHeader}>
           {columns.map(col => (
-            <td key={col.key}>{row[col.dataIndex]}</td>
+            <Cell key={col.key} width="100px">
+              {col.title}
+            </Cell>
           ))}
-        </tr>
-      ))}
-    </tbody>
-  </table>
-);
+        </TableHeader>
+        <TableBody>
+          {rows.map(row => (
+            <Row key={row.key}>
+              {columns.map(col => (
+                <Cell key={col.key} width="100px">
+                  {row[col.dataIndex]}
+                </Cell>
+              ))}
+            </Row>
+          ))}
+        </TableBody>
+      </Container>
+    );
+  }
+
+  componentDidMount() {
+    const { tableContainer, tableHeader } = this;
+    tableContainer.current.addEventListener(
+      'scroll',
+      ({ target: { scrollTop, scrollLeft } }) => {
+        tableHeader.current.style.top = `${scrollTop}px`;
+        tableHeader.current.style.left = `${scrollLeft}px`;
+      },
+    );
+  }
+}
 
 Table.defaultProps = {
   columns: [],
--- src/index.js
+++ src/index.js
@@ -3,18 +3,31 @@ import ReactDOM from 'react-dom';
 import { Table } from './components';
 
 ReactDOM.render(
-  <Table
-    columns={[
-      { key: '1', title: 'COL_1', dataIndex: '1' },
-      { key: '2', title: 'COL_2', dataIndex: '2' },
-      { key: '3', title: 'COL_3', dataIndex: '3' },
-    ]}
-    rows={[
-      { key: '1', 1: '11', 2: '12', 3: '13' },
-      { key: '2', 1: '21', 2: '22', 3: '23' },
-    ]}
-    width="500px"
-    height="500px"
-  />,
+  <div
+    style={{
+      width: '600px',
+      height: '600px',
+      border: '1px solid blue',
+      boxSizing: 'border-box',
+    }}
+  >
+    <Table
+      columns={[
+        { key: '1', title: 'COL_1', dataIndex: '1' },
+        { key: '2', title: 'COL_2', dataIndex: '2' },
+        { key: '3', title: 'COL_3', dataIndex: '3' },
+      ]}
+      rows={[
+        ...[...Array(30)].map((v, i) => ({
+          key: `${i + 1}`,
+          1: `${i + 1}-1`,
+          2: `${i + 1}-2`,
+          3: `${i + 1}-3`,
+        })),
+      ]}
+      width="100%"
+      height="100%"
+    />
+  </div>,
   document.getElementById('root'),
 );
