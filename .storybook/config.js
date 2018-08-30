d2fb25b3cae7f0c0e06f9effb9b4efee4381364c
daengdaenglee <gunho1020@gmail.com>
2018-08-30 09:27:52 +0900

    TST: Add storybook

---
+++ .storybook/config.js
@@ -0,0 +1,11 @@
+import { configure } from '@storybook/react';
+
+const req = require.context('../src/stories', true, /\.stories\.(js|jsx)$/);
+
+function loadStories() {
+  req.keys().forEach(filename => {
+    req(filename);
+  });
+}
+
+configure(loadStories, module);
