f0d6ac33b7c7d64deac6daca8a3e7747dabae1e1
daengdaenglee <gunho1020@gmail.com>
2018-08-30 13:47:09 +0900

    TST: Make infinite scroll story

--- src/stories/advanced.stories.js
+++ src/stories/advanced.stories.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import StressTable from './stress-table';
+import InfiniteTable from './infinite-table';
 
-storiesOf('02-Advanced', module).add('stress-menu table', () => (
-  <StressTable />
-));
+storiesOf('02-Advanced', module)
+  .add('stress-menu table', () => <StressTable />)
+  .add('infinite table', () => <InfiniteTable />);
