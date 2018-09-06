cd7b55d8713b29df32d7285603841c61567a9755
daengdaenglee <gunho1020@gmail.com>
2018-09-06 13:41:55 +0900

    TST: Update infinite table story

--- src/stories/infinite-table.js
+++ src/stories/infinite-table.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Table } from '../components';
 
 const generateColumns = num =>
@@ -17,6 +17,22 @@ const generateRows = (rowNum, colNum, start) =>
     return obj;
   });
 
+const ContextMenu = ({ onOff, x, y, colKey, rowKey }) =>
+  onOff ? (
+    <div
+      style={{
+        position: 'absolute',
+        top: y,
+        left: x,
+        width: '180px',
+        height: '30px',
+        border: '1px solid black',
+        boxSizing: 'border-box',
+        backgroundColor: 'skyblue',
+      }}
+    >{`row: ${rowKey}, col: ${colKey}`}</div>
+  ) : null;
+
 class InfiniteTable extends Component {
   constructor(props) {
     super(props);
@@ -24,25 +40,94 @@ class InfiniteTable extends Component {
       columns: generateColumns(20),
       rows: generateRows(30, 20, 1),
       curMaxRow: 30,
+      selectedRows: [],
+      selectedCols: [],
+      contextMenu: {
+        onOff: false,
+        x: 0,
+        y: 0,
+        row: '',
+        col: '',
+      },
     };
     this.scroller = React.createRef();
+    this._onClickCell = this._onClickCell.bind(this);
+    this._onContextMenu = this._onContextMenu.bind(this);
     this._onScroll = this._onScroll.bind(this);
     this._addMoreRows = this._addMoreRows.bind(this);
   }
 
   render() {
-    const { _onScroll, scroller } = this;
-    const { columns, rows } = this.state;
+    const { _onClickCell, _onContextMenu, _onScroll, scroller } = this;
+    const {
+      columns,
+      rows,
+      selectedCols,
+      selectedRows,
+      contextMenu: { onOff, x, y, row, col },
+    } = this.state;
     return (
-      <Table
-        width="500px"
-        height="500px"
-        columns={columns}
-        rows={rows}
-        onScroll={_onScroll}
-        setScrollRef={scroller}
-      />
+      <Fragment>
+        <Table
+          width="500px"
+          height="500px"
+          columns={columns}
+          rows={rows}
+          selectedCols={selectedCols}
+          selectedRows={selectedRows}
+          onClickCell={_onClickCell}
+          onContextMenu={_onContextMenu}
+          onScroll={_onScroll}
+          setScrollRef={scroller}
+        />
+        <ContextMenu onOff={onOff} x={x} y={y} rowKey={row} colKey={col} />
+      </Fragment>
     );
+  }
+
+  _onClickCell({ type, col, row }) {
+    switch (type) {
+    case 'col':
+      this.setState({
+        selectedCols: [col.key],
+        selectedRows: [],
+        contextMenu: {
+          onOff: false,
+          x: 0,
+          y: 0,
+          row: '',
+          col: '',
+        },
+      });
+      break;
+    case 'cell':
+      this.setState({
+        selectedCols: [],
+        selectedRows: [row.key],
+        contextMenu: {
+          onOff: false,
+          x: 0,
+          y: 0,
+          row: '',
+          col: '',
+        },
+      });
+      break;
+    default:
+    }
+  }
+
+  _onContextMenu({ event, row, col }) {
+    event.preventDefault();
+    this.setState({
+      contextMenu: {
+        onOff: true,
+        x: event.clientX,
+        y: event.clientY,
+        row: row ? row.key : 'None',
+        col: col.key,
+      },
+    });
   }
 
   _onScroll({ target: { scrollTop, offsetHeight, scrollHeight } }) {
