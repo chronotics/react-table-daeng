9b02e0b1ed80dfd62d306e5959332f6ac6995bfb
daengdaenglee <gunho1020@gmail.com>
2018-09-14 09:59:06 +0900

    BLD: Add TableHead component to exported modules

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Cell from './1-atoms/cell';
 import Container from './1-atoms/container';
 import Row from './1-atoms/row';
 import TableBody from './1-atoms/table-body';
+import TableHead from './1-atoms/table-head';
 import Table from './2-molecules/table';
 
-export { Cell, Container, Row, TableBody, Table };
+export { Cell, Container, Row, TableBody, TableHead, Table };
