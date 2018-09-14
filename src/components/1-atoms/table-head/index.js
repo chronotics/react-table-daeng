9c52018052b3092e881d0726af896fe56a46a0c9
daengdaenglee <gunho1020@gmail.com>
2018-09-14 09:58:34 +0900

    CLN: Add prop types to TableHead component

--- src/components/1-atoms/table-head/index.js
+++ src/components/1-atoms/table-head/index.js
@@ -1,11 +1,39 @@
-import styled from 'styled-components';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const TableHead = styled.div.attrs({
-  style: props => ({ height: props.height }),
-})`
-  position: absolute;
-  width: 100%;
-  display: flex;
-`;
+class TableHead extends Component {
+  render() {
+    const { height, style, children, innerRef, ...props } = this.props;
+    return (
+      <div
+        ref={innerRef}
+        style={{
+          height,
+          position: 'absolute',
+          widht: '100%',
+          display: 'flex',
+          ...style,
+        }}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  }
+}
+
+TableHead.defaultProps = {
+  height: '40px',
+  style: {},
+  children: null,
+  innerRef: () => {},
+};
+
+TableHead.propTypes = {
+  height: PropTypes.string,
+  style: PropTypes.object,
+  children: PropTypes.node,
+  innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+};
 
 export default TableHead;
