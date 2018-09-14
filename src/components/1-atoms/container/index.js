fe1c7b0a4f6d2e2e85590404194eab3808842a6e
daengdaenglee <gunho1020@gmail.com>
2018-09-14 09:37:49 +0900

    CLN: Add prop types to Container component

--- src/components/1-atoms/container/index.js
+++ src/components/1-atoms/container/index.js
@@ -1,13 +1,44 @@
-import styled from 'styled-components';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const Container = styled.div.attrs({
-  style: props => ({ width: props.width, height: props.height }),
-})`
-  overflow: auto;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  background-color: transparent;
-`;
+class Container extends Component {
+  render() {
+    const { width, height, style, children, innerRef, ...props } = this.props;
+    return (
+      <div
+        ref={innerRef}
+        style={{
+          width,
+          height,
+          overflow: 'auto',
+          position: 'relative',
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: 'transparent',
+          ...style,
+        }}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  }
+}
+
+Container.defaultProps = {
+  width: '600px',
+  height: '600px',
+  style: {},
+  children: null,
+  innerRef: () => {},
+};
+
+Container.propTypes = {
+  width: PropTypes.string,
+  height: PropTypes.string,
+  style: PropTypes.object,
+  children: PropTypes.node,
+  innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+};
 
 export default Container;
